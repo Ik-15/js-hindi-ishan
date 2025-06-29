884a90029dc4a94346189a3eb77dabc30bd96997
Ik-15 <kamraik2020@gmail.com>
2025-06-29 17:24:09 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId=1234567
+let accountEmail="ishan151206@gmail.com"
+var accountPassword="12345"
+accountcity="Sirsa"
+console.table([accountId,accountEmail,accountPassword,accountcity])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code asnewer version
+// alert(3+3) we are using node js not browser 
+ console.log(3+3)
+
+ console.log("Ishan kamra")
+
+ let name="Ishan kamra"
+ let age=18
+ let isloggedin=true
+//  number= 2 to the power 53 range hoti hai
+// big int
+// string=""
+// Boolean= true or false
+// null= standalone value 
+// undefined=
+// symbol= unique
+// object
+console.log(typeof undefined);
+// null type object or undefined type undefined
