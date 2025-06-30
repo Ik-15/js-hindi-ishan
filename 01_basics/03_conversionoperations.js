99d98a4fd50560f5968725cc7bfaa38b8bcfbdfc
Ik-15 <kamraik2020@gmail.com>
2025-06-30 11:37:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,23 @@
+let score ="33abc"
+
+console.log(typeof score);
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// "33" = 33
+// "33abc"=NaN.   not a number 
+// true = 1. false = 0
+
+let isloggedin=0
+let BooleanIsloggedin=Boolean(isloggedin)
+console.log(BooleanIsloggedin);
+
+// 1= true. 0= false
+// ""= false.  "ishan"=true
+
+let someNumber="ishan"
+
+let StringNumber=String(someNumber)
+console.log(StringNumber);
