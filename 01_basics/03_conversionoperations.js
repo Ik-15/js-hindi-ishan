92af07de0aa17eecc57e21c0680ff6c1e518986c
Ik-15 <kamraik2020@gmail.com>
2025-07-01 07:08:50 +0000

    datatype summary

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -21,3 +21,23 @@ let someNumber="ishan"
 
 let StringNumber=String(someNumber)
 console.log(StringNumber);
+
+
+//***************OPERATIONS******************// 
+
+
+
+let str1="hello"
+let str2=" ishan"
+let str3= str1+str2;
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1+ "2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(2+2+"1");
+console.log((3+4)*5%3);
+
+
+
---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,20 @@
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2">1);
+
+console.log(null>0);
+
+console.log(null>=0);
+
+// === strict check
+
+console.log("2"==2);// value of number is equal
+console.log("2"===2);// different datatype
+
+console.log(false> true);
