86cb7de0f66a6a79d4bcdc02fd74e467d92a1f9e
Ik-15 <kamraik2020@gmail.com>
2025-07-01 07:09:14 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,45 @@
+//primitive
+
+// 7 Types => string,number, boolean, null,undefined,symbol,BigInt
+
+
+
+//JavaScript is a dynamically typed language.
+//This means that variable types are checked during runtime (when the code is executed) 
+// rather than at compile time.
+
+
+const score=100
+const scorevalue=100.23
+
+const isloggedin=false
+const outsidetemp=null
+
+
+let useremail; // the value is undefined
+
+const id = Symbol('123')
+const Anotherid= Symbol('123') // symbol show an unique id of thye variable
+
+console.log(id===Anotherid);
+
+const Bignumber=1234567890n
+
+
+
+// Reference. or Non primtive 
+
+// Array ,Objects, Functions
+
+const heroes=["shaktiman","naagraj","doga"]
+ let myobj = {
+    name: ishan,
+    age: 18,
+}
+
+const myfunction = function(){
+
+    console.log("HELLO WORLD");
+
+    
+}
